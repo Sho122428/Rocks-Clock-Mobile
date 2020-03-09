4108890cbeb07c2cc732b9c73f98b7d1573a0aff
Florencio Baroman <fbaroman@fullscale.io>
2020-03-09 17:34:09 +0800

    Add project files.

---
+++ Rocks-Clock-Mobile/Rocks-Clock-Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Rocks_Clock_Mobile.Models;
+
+namespace Rocks_Clock_Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
