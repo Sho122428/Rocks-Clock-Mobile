4ef74507d0e1747beb5ddf2fcd15dbb6d3ad4fa5
Janno Pono <jpono@fullscale.io>
2020-03-09 19:47:17 +0800

    test commit master

---
+++ RockClockMobile/RockClockMobile/RockClockMobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using RockClockMobile.Models;
+
+namespace RockClockMobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
