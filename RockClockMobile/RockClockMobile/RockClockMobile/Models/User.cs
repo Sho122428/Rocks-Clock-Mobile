65feb53bcaf6e0100cbb436f1c2b6f8cd09cbce7
Florencio Baroman <fbaroman@fullscale.io>
2020-03-09 22:22:44 +0800

    added user model

---
+++ RockClockMobile/RockClockMobile/RockClockMobile/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockClockMobile.Models
+{
+    public class User
+    {
+        string username;
+        string password;
+        public string Username 
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+    }
+}
