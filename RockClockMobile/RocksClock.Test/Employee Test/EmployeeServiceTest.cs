ae97d4fd39712b7a27afd140f8cae4bcb99fb61e
Florencio Baroman <fbaroman@fullscale.io>
2020-03-20 21:04:16 +0800

    added test

---
+++ RockClockMobile/RocksClock.Test/Employee Test/EmployeeServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using RockClockMobile;
+using RockClockMobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RocksClock.Test.Employee_Test
+{
+    public class EmployeeServiceTest
+    {
+        [Fact]
+        public void Employee_Expect_HasData()
+        {
+            LoginViewModel loginVM = new LoginViewModel();
+
+            var dd = loginVM.Employees();
+
+            Assert.Empty(dd);
+        }
+       
+    }
+}
---
+++ RockClockMobile/RocksClock.UI.Test/Tests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using RockClockMobile;
+using RockClockMobile.Views;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace RocksClock.UI.Test
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void WelcomeTextIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("Welcome to Xamarin.Forms!"));
+            app.Screenshot("Welcome screen.");
+
+            PincodePage pincodePage = new PincodePage();
+            LoginPage loginPage = new LoginPage();
+            
+            //app.Tap(c => c.a);
+
+            ////var ff = app.Tap(loginPage.btnLogin);
+            //Assert.Equals(results.Any());
+        }
+    }
+}
